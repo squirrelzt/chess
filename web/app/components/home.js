585b5eabcd40d4b4dd854183fd11f57806927781
squirrelzt <squirrelzt@126.com>
2020-04-18 17:37:11 +0800

    删除多个变量

--- web/app/components/home.js
+++ web/app/components/home.js
@@ -47,41 +47,6 @@ export default class Home extends Component {
             selectedItem: '',
             selectedItemBackgroudColor: '#6387ea',
             data: [],
-            item11: '',
-            item12: '',
-            item13: '',
-            item14: '',
-            item15: '',
-            item16: '',
-            item17: '',
-            item18: '',
-    
-            item21: '',
-            item22: '',
-            item23: '',
-            item24: '',
-            item25: '',
-            item26: '',
-            item27: '',
-            item28: '',
-    
-            item31: '',
-            item32: '',
-            item33: '',
-            item34: '',
-            item35: '',
-            item36: '',
-            item37: '',
-            item38: '',
-    
-            item41: '',
-            item42: '',
-            item43: '',
-            item44: '',
-            item45: '',
-            item46: '',
-            item47: '',
-            item48: '',
         }
     }
     
@@ -101,50 +66,13 @@ export default class Home extends Component {
         data.forEach(element => {
             items[element.x-1][element.y-1] = element;
         });
-        // console.log('==========')
-        // console.log(items)
         this.setState({
             items
-        })
-
-        data.forEach(element => {
-            if ('11' == element.location) {
-                this.setState({
-                    item11: element
-                })
-            } else if ('12' == element.location) {
-                this.setState({
-                    item12: element
-                })
-            } else if ('13' == element.location) {
-                this.setState({
-                    item13: element
-                })
-            } else if ('14' == element.location) {
-                this.setState({
-                    item14: element
-                })
-            } else if ('15' == element.location) {
-                this.setState({
-                    item15: element
-                })
-            } else if ('16' == element.location) {
-                this.setState({
-                    item16: element
-                })
-            } else if ('17' == element.location) {
-                this.setState({
-                    item17: element
-                })
-            } else if ('18' == element.location) {
-                this.setState({
-                    item18: element
-                })
-            }
         })
     }
 
     componentWillMount(){
+        // console.log(localStorage.getItem('username'))
         this.fetch();
     }
 
@@ -194,57 +122,11 @@ export default class Home extends Component {
     render() {
         // console.log('+++++++++++++++')
         // console.log(this.state.items)
-        const { item11, item12, item13, item14, item15, item16, item17, item18,
-                item21, item22, item23, item24, item25, item26, item27, item28,
-                item31, item32, item33, item34, item35, item36, item37, item38,
-                item41, item42, item43, item44, item45, item46, item47, item48, items, selectedItem, selectedItemBackgroudColor } = this.state;
+        const { items, selectedItem, selectedItemBackgroudColor } = this.state;
         return (
             <div className="btn-margin">
                 <div className="monitor-frame">
                     <div className="chess-container">
-                        <Row>
-                            <Col span={3}>
-                                <Button shape="circle" type='primary'
-                                    onClick={this.onSelect.bind(this,item11)}>
-                                    {item11.state == 'DISPLAY' ? item11.name : ''}
-                                </Button>
-                            </Col>
-                            <Col span={3}>
-                                <Button shape="circle">
-                                {item12.state == 'DISPLAY' ? item12.name : ''}
-                                </Button>
-                            </Col>
-                            <Col span={3}>
-                                <Button shape="circle">
-                                    {item13.state == 'DISPLAY' ? item13.name : ''}
-                                </Button>
-                            </Col>
-                            <Col span={3}>
-                                <Button shape="circle">
-                                    {item14.state == 'DISPLAY' ? item14.name : ''}
-                                </Button>
-                            </Col>
-                            <Col span={3}>
-                                <Button shape="circle">
-                                    {item15.state == 'DISPLAY' ? item15.name : ''}
-                                </Button>
-                            </Col>
-                            <Col span={3}>
-                                <Button shape="circle">
-                                    {item16.state == 'DISPLAY' ? item16.name : ''}
-                                </Button>
-                            </Col>
-                            <Col span={3}>
-                                <Button shape="circle">
-                                    {item17.state == 'DISPLAY' ? item17.name : ''}
-                                </Button>
-                            </Col>
-                            <Col span={3}>
-                                <Button shape="circle">
-                                    {item18.state == 'DISPLAY' ? item18.name : ''}
-                                </Button>
-                            </Col>
-                        </Row>
                         {items.map((rowItem,rowIndex)=>{
                             return (
                                 <Row key={rowIndex}>
