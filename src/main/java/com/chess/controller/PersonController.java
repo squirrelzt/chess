2307af754f968ab95d1bd6edbe8c5b18b15db5de
squirrelzt <squirrelzt@126.com>
2020-05-18 17:08:33 +0800

    @Valid注解校验参数

--- src/main/java/com/chess/controller/PersonController.java
+++ src/main/java/com/chess/controller/PersonController.java
@@ -5,8 +5,10 @@ import com.chess.service.ChessService;
 import com.chess.service.PersonService;
 import com.chess.vo.request.LoginRequestVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -32,9 +34,15 @@ public class PersonController {
     }
 
     @PostMapping("logins")
-    public Map login(@RequestBody LoginRequestVO vo) {
-        Person person = personService.login(vo.getUsername(), vo.getPassword());
+    public Map login(@RequestBody @Valid LoginRequestVO vo, BindingResult result) {
         Map<String, Object> map = new HashMap<>(2);
+        if (result.hasErrors()) {
+            map.put("result", 1);
+            map.put("message", result.getAllErrors().get(0).getDefaultMessage());
+            return map;
+        }
+        Person person = personService.login(vo.getUsername(), vo.getPassword());
+
         if (Objects.isNull(person)) {
             map.put("result", 1);
         } else {
--- src/main/java/com/chess/vo/request/LoginRequestVO.java
+++ src/main/java/com/chess/vo/request/LoginRequestVO.java
@@ -2,10 +2,15 @@ package com.chess.vo.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 public class LoginRequestVO implements Serializable {
+
+    @NotNull(message = "用户名不能为空")
     private String username;
+
+    @NotNull(message = "密码不能为空")
     private String password;
 }
